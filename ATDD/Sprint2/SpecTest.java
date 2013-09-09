e2430721a7e00fa8f1c53787fd99f09ba4e38420
cinoter <cinoter@hotmail.com>
2013-09-09 13:20:54 -0600

    Update SpecTest.java
    prueba fallida

--- ATDD/Sprint2/SpecTest.java
+++ ATDD/Sprint2/SpecTest.java
@@ -3,7 +3,32 @@ package Sprint2;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
+
+import com.example.gestoreconocimo.Categoria;
+
+
 @RunWith(ConcordionRunner.class)
+/**
+ * Clase encargada de realizar las pruebas de aceptacion 
+ * del Sprint No. 2
+ * @author CINOTER
+ *
+ */
 public class SpecTest {
-	//prueba
+
+//comentario
+		
+	public boolean getValidacionCategoriaNueva(String categoria)
+	{
+	
+		Categoria cat = new Categoria();
+		
+		return	cat.funcion_agregar_nombre(categoria);
+		
+	}
+	
+	public boolean getPrueba()
+	{
+		return true;
+	}
 }
